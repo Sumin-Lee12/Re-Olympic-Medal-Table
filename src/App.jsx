073fd54268a12c5700af62415a81bdf86978f3db
Sumin-Lee12 <leesumin041@gmail.com>
2025-01-27 12:14:13 +0900

    list와 각 버튼들 구현

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,60 @@ function App() {
   const [silver, setSilver] = useState(0);
   const [bronze, setBronze] = useState(0);
 
+  const resetForm = () => {
+    setCountry("");
+    setGold(0);
+    setSilver(0);
+    setBronze(0);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    const existingMedal = medals.find((medal) => medal.country === country)
+    
+    if (!existingMedal) {
+      const newMedal = {
+        country: country,
+        gold: parseInt(gold),
+        silver: parseInt(silver),
+        bronze: parseInt(bronze),
+      }
+  
+      setMedals([...medals, newMedal]);
+      resetForm();
+    } else {
+      alert("이미 존재하는 국가입니다!")
+    }
+  };
+
+  const handleDelete = (country) => {
+    const updatedMedals = medals.filter((medal) => medal.country !== country)
+    return updatedMedals
+  };
+
+  const handleUpdate = () => {
+    const existingMedal = medals.find((medal) => medal.country === country)
+    
+    if (existingMedal) {
+      const newMedal = {
+        country: country,
+        gold: parseInt(gold),
+        silver: parseInt(silver),
+        bronze: parseInt(bronze),
+      }
+      setMedals([newMedal]); //()안에 꼭! [] 넣기
+      resetForm();
+    } else {
+      alert("업데이트할 국가가 존재하지 않습니다!")
+    }
+  }
 
   return (
     <>
       <div className='app'>
         <h1 className='title'>파리 올림픽 메달 트래커!</h1>
-        <form className='medal-form'>
+        <form onSubmit={handleSubmit} className='medal-form'>
           <label className='input-wrapper'>
             국가 이름
             <input 
@@ -58,8 +106,23 @@ function App() {
             max={99}
             />
           </label>
-          <button >추가하기</button>
+          <button type='submit'>추가하기</button>
+          <button type='button' onClick={handleUpdate}>업데이트</button>
         </form>
+
+        <ul className='medal-list'>
+          {medals.map((medal) => {
+            return (                  // 삭제 버튼이 작동 안됌....왜지...?
+            <li key={medal.country}>
+              <span>{medal.country}</span>
+              <span>🥇 {medal.gold}</span>
+              <span>🥈 {medal.silver}</span>
+              <span>🥉 {medal.bronze}</span>
+              <button onClick={() => handleDelete(medal.country)}>삭제</button> 
+            </li>
+            )
+          })}
+        </ul>
       </div>
     </>
   )
